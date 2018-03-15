c615549a4baa94395c772ca07257f762e99c3d84
Vinicius <vncastanheira@gmail.com>
2018-03-14 22:49:09 -0300

    finished Brush class

--- Brush.cs
+++ Brush.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace csg_NET
 {
@@ -14,19 +10,102 @@ namespace csg_NET
 
         public Brush GetNext { get { return next; } }
         public Poly GetPolys { get { return polys; } }
-        public int GetNumberOfPolys { get; }
-        public int GetNumberOfBrushes { get; }
-        public bool IsLast { get; }
+
+        public int GetNumberOfPolys
+        {
+            get
+            {
+                Poly poly = polys;
+                int count = 0;
+                while (poly != null)
+                {
+                    poly = poly.GetNext;
+                    count++;
+                }
+                return count;
+            }
+        }
+
+        public int GetNumberOfBrushes
+        {
+            get
+            {
+                Brush brush = next;
+                int count = 1;
+                while (brush != null)
+                {
+                    brush = brush.GetNext;
+                    count++;
+                }
+                return count;
+            }
+        }
+
+        public bool IsLast { get { return next == null; } }
 
         public Brush()
         {
-
+            next = null;
+            polys = null;
         }
 
-        public Brush CopyList() { }
+        public Brush CopyList()
+        {
+            Brush brush = new Brush();
+            brush.max = max;
+            brush.min = min;
 
-        public void SetNext(Brush brush) { }
-        public void AddPoly(Poly poly) { }
+            brush.polys = polys.CopyList();
+
+            if (!IsLast)
+            {
+                brush.SetNext(next.CopyList());
+            }
+
+            return brush;
+        }
+
+        public void SetNext(Brush brush)
+        {
+            if(IsLast)
+            {
+                next = brush;
+                return;
+            }
+
+            if(brush == null)
+            {
+                next = null;
+            }
+            else
+            {
+                Brush b = brush;
+                while (!b.IsLast)
+                {
+                    b = b.GetNext;
+                }
+
+                b.SetNext(next);
+                next = brush;
+            }
+        }
+
+        public void AddPoly(Poly poly)
+        {
+            if (polys == null)
+            {
+                polys = poly;
+                return;
+            }
+
+            Poly p = polys;
+            while (!p.IsLast)
+            {
+                p = p.GetNext;
+            }
+
+            p.SetNext(poly);
+        }
 
         public Poly MergeList()
         {
@@ -110,6 +189,7 @@ namespace csg_NET
                 }
             }
 
+            return polyList;
         }
 
         public void ClipToBrush(Brush brush, bool clipOnPlane)
@@ -136,8 +216,72 @@ namespace csg_NET
             polys = polyList;
         }
 
-        public void CalculateAABB() { }
-        public bool AABBIntersect(Brush brush) { }
+        public void CalculateAABB()
+        {
+            min = polys.verts[0].p;
+            max = polys.verts[0].p;
 
+            Poly p = polys;
+
+            for (int i = 0; i < GetNumberOfPolys; i++)
+            {
+                for (int j = 0; j < p.GetNumberOfVertices; j++)
+                {
+                    // Calculate min
+                    if (p.verts[j].p.x < min.x)
+                    {
+                        min.x = p.verts[j].p.x;
+                    }
+
+                    if (p.verts[j].p.y < min.y)
+                    {
+                        min.y = p.verts[j].p.y;
+                    }
+
+                    if (p.verts[j].p.z < min.z)
+                    {
+                        min.z = p.verts[j].p.z;
+                    }
+
+                    // Calculate max
+                    if (p.verts[j].p.x > max.x)
+                    {
+                        max.x = p.verts[j].p.x;
+                    }
+
+                    if (p.verts[j].p.y > max.y)
+                    {
+                        max.y = p.verts[j].p.y;
+                    }
+
+                    if (p.verts[j].p.z > max.z)
+                    {
+                        max.z = p.verts[j].p.z;
+                    }
+                }
+
+                p = p.GetNext;
+            }
+        }
+
+        public bool AABBIntersect(Brush brush)
+        {
+            if((min.x > brush.max.x) || (brush.min.x > max.x))
+            {
+                return false;
+            }
+
+            if ((min.y > brush.max.y) || (brush.min.y > max.y))
+            {
+                return false;
+            }
+
+            if ((min.z > brush.max.z) || (brush.min.z > max.z))
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
